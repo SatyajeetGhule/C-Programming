6084f2292477d694b527c1bfa9ac1544349994a1
SatyajeetGhule <134911172+SatyajeetGhule@users.noreply.github.com>
2025-11-17 14:35:51 +0530

    Update Program16_1.c

--- Assignment/Assignment_16/Program16_1.c
+++ Assignment/Assignment_16/Program16_1.c
@@ -1,1 +1,104 @@
+////////////////////////////////////////////////////////////////////////////
+//
+//  Request Header File 
+//
+////////////////////////////////////////////////////////////////////////////
 
+#include<stdio.h>
+#include<stdlib.h>
+typedef int * IPTR;
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name :    CountEven
+//  Description   :    Accept a Number from user and return frequency of
+//                     Even Number.
+//  Input         :    Integer iPtr[], Integer
+//  Output        :    Integer
+//  Author        :    Satyajeet Manohar Ghule
+//  Date          :    15/11/2025
+//
+////////////////////////////////////////////////////////////////////////////
+
+int CountEven(int Arr[], int iSize)
+{
+    int iCnt = 0, iCount = 0;
+
+    for(iCnt = 0; iCnt < iSize; iCnt++)
+    {
+        if((Arr[iCnt] % 2 ) == 0)
+        {
+            iCount++;
+        }
+    }
+    return iCount;
+}
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Entry point Function for the Application
+//
+////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iLength = 0,iCnt = 0, iRet = 0;    
+    IPTR iPtr = NULL;
+
+    printf("Enter a Number of Element :\n");
+    scanf("%d",&iLength);
+
+    iPtr = (IPTR)malloc(iLength * sizeof(int));
+
+    if(iPtr == NULL)
+    {
+        printf("Unable to allocate memory\n");
+        return -1;
+    }
+
+    for(iCnt= 0; iCnt < iLength; iCnt++)
+    {
+        printf("Enter a %d Element :\n",iCnt+1);
+        scanf("%d",&iPtr[iCnt]);
+    }
+
+    iRet = CountEven(iPtr, iLength);
+    printf("Result is : %d",iRet);
+
+    free(iPtr);
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////
+//
+//  Test Cases Successfully Handeld by This Application
+//
+//  Input   :                               Output  :
+//      Enter a Number of Element :            Result is : 3
+//      6
+//      Enter a 1 Element :
+//      85
+//      Enter a 2 Element :
+//      66
+//      Enter a 3 Element :
+//      03
+//      Enter a 4 Element :
+//      80
+//      Enter a 5 Element :
+//      93
+//      Enter a 6 Element :
+//      88
+//
+//  Input   :                               Output  :
+//      Enter a Number of Element :            Result is : 3
+//      5                                           
+//      Enter a 1 Element :
+//      12
+//      Enter a 2 Element :
+//      25
+//      Enter a 3 Element :
+//      29
+//      Enter a 4 Element :
+//      34
+//      Enter a 5 Element :
+//      15
+////////////////////////////////////////////////////////////////////////////
