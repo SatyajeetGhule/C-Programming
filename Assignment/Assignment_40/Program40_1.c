2fdeaf53f34c89768f5ef2d6fb4daee2a979a742
SatyajeetGhule <134911172+SatyajeetGhule@users.noreply.github.com>
2025-12-31 15:50:23 +0530

    Implement linked list with insertion and display functions
    This program implements a linked list with functions to insert nodes at the beginning and display nodes greater than a given number.

---
+++ Assignment/Assignment_40/Program40_1.c
@@ -0,0 +1,141 @@
+////////////////////////////////////////////////////////////////////////////
+//
+//  Header Files
+//
+////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Structure Definition
+//
+////////////////////////////////////////////////////////////////////////////
+
+struct node
+{
+    int Data;
+    struct node *Next;
+};
+
+typedef struct node NODE;
+typedef struct node* PNODE;
+typedef struct node** PPNODE;
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : InsertFirst
+//  Description   : Insert node at beginning of linked list
+//  Input         : Pointer to pointer of head, Integer
+//  Output        : None
+//  Author        : Satyajeet Manohar Ghule
+//  Date          : 31/12/2025
+//
+////////////////////////////////////////////////////////////////////////////
+
+void InsertFirst(PPNODE first , int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->Data = no;
+    newn->Next = NULL;
+
+    if (*first == NULL)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn->Next = *first;
+        *first = newn;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : DisplayGreater
+//  Description   : Display on Screen Count Numbers Greater than given number.
+//  Input         : Pointer to head
+//  Output        : Display on screen
+//  Author        : Satyajeet Manohar Ghule
+//  Date          : 31/12/2025
+//
+////////////////////////////////////////////////////////////////////////////
+
+void DisplayGreater(PNODE first, int no)
+{
+    printf("In List %d number Greater than numbers are : \n",no);
+    while (first != NULL)
+    {
+        if(first->Data > no)
+        {
+           printf("| %d |-> ",first->Data);
+        }
+        first = first->Next;
+    }
+    printf("\n");
+}
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description   : Display All Node
+//  Input         : Pointer to head
+//  Output        : Display on screen
+//  Author        : Satyajeet Manohar Ghule
+//  Date          : 31/12/2025
+//
+////////////////////////////////////////////////////////////////////////////
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d | -> ",first->Data);
+        first = first->Next;
+    }
+    printf("NULL\n");
+}
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Entry Point Function
+//
+////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+    PNODE head = NULL;
+
+    InsertFirst(&head,20);
+    InsertFirst(&head,33);
+    InsertFirst(&head,26);
+    InsertFirst(&head,25);
+    InsertFirst(&head,20);
+    InsertFirst(&head,29);    
+    Display(head);
+
+    printf("Enter a Number :\n");
+    scanf("%d",&iValue);
+    DisplayGreater(head,iValue);   
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////
+//
+//  Test Cases
+//
+//  Input  : |29|->|20|->|25|->|26|->|33|->|22|->NULL
+//  
+//  Output :
+//           Enter a Number : 26
+//
+//           In List 20 number Greater than numbers are : 
+//           | 29 |-> | 25 |-> | 26 |-> | 33 |->
+//
+////////////////////////////////////////////////////////////////////////////
